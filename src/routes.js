68820dfe437fa74f2e12d868edbfaaa5cbff3316
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-05-26 20:11:44 -0300

    Iniciando projeto

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+// export class Route{
+//     routes ={
+//     }
+//     add (pathName,page){
+//         this.routes[pathName] = page
+//     }
+//     route(event){
+//         event = event || window.event
+//         event.
+
+//         window.history.pushState(null,'',event.target)
+//     }
+
+// }
